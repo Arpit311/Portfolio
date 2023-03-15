144b302719390a36424d813fcde23bcff6e9cbd7
Arpit K. Bhatnagar <112899633+Arpit311@users.noreply.github.com>
2023-03-15 21:26:04 +0530

    Add files via upload

---
+++ Jav.js
@@ -0,0 +1,9 @@
+document.getElementsByTagName("p")[0].innerHTML = "I love to watch web series and movies a lot. Usually I prefer action and thriller genre but i rarely go through any horror screenplay.<br> My favourite creator the Marvel Studio. I litrally watch every web series and movie by them.<br>Hope you would be also liking this genres.";
+
+document.getElementsByTagName("p")[1].innerHTML = "I love to play badminton. I statrted my badminton journey when I was in class 7th. from there, till today i have the same affection to this game.<br>I have participated in many competions till now and have hopes that I will continue till I can.<br>Still love to play the game with same energy.";
+
+document.getElementsByTagName("p")[2].innerHTML = "From the day I came to know about the coding and its various prospectives, I always try from my side to deliever the best efforts for it.<br>I usually compete in coding contests and learn new concepts. Web Dev is the latest skill in this domain of which i am fond of.";
+
+document.getElementsByTagName("h2")[0].innerHTML = " Movies and Web series";
+document.getElementsByTagName("h2")[1].innerHTML = " Badminton";
+document.getElementsByTagName("h2")[2].innerHTML = " Coding";
---
+++ JavI.js
@@ -0,0 +1,5 @@
+document.getElementsByTagName("p")[0].innerHTML = " Let me introduce you to Myself first.<br />Here I Arpit Bhatnagar with you. Hope you are doing well. I am aresident of Haridwar, Uttrakhand.<br />I love to interact with people.I am fond of Travelling through places and Explore new things. So see you soon too.";
+
+document.getElementsByTagName("p")[1].innerHTML = "My Primitive learning took place in Jamnagar,Gujrat. And later i moved to Haridwar,Uttrakhand.<br/>From class 6th I joined JNV's where i completly got exposure to hostel life.<br />Currently I am pursuing my B.Tech course in IT sector from prestigious NIT Raipur, Chattisgarh.";
+
+document.getElementsByTagName("p")[2].innerHTML = "I love to do and participate in many versatile activities.<br />I like to play Badminton a lot and I have represented my school at regional(UK+ UP)<br> for three times in the same sport.<br>I love to code and solve problems whenever i get a chance to do this part. <br />I am also good in communication.";
